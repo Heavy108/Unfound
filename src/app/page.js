686cd8ce0dc9a81754a8e753075d3274d2c2c415
23f1000758 <heavy1089@gmail.com>
2025-07-23 19:57:59 +0530

    added animation

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,42 @@
 import Image from "next/image";
 import Navbar from "@/components/navbar";
-import Gradient1 from "@/assets/Gradient.webp";
-
+import Gradient1 from "@/assets/Gradient.png";
+import style from "@/css/Home.module.css"
+import { FaArrowRight } from "react-icons/fa6";
+import Spline from "@splinetool/react-spline/next";
 export default function Home() {
   return (
     <>
-      <div className="relative  h-[500px] w-full overflow-hidden ">
+      <div className="relative  h-[400px] w-full overflow-hidden ">
         {/* Background image */}
         <Image src={Gradient1} alt="gradient1" fill className="z-0" priority />
 
         {/* Navbar on top */}
-        <div className="relative z-10">
+        <div className="relative z-10 mb-[3rem]">
           <Navbar />
         </div>
+
+        <div className={style.Hero}>
+          <h1>
+            Where Ideas Become <br /> Digital Experiences
+          </h1>
+          <p>
+            We collaborate with forward-thinking brands to turn their ideas into
+            digital products and stories.
+          </p>
+          <div className={style.buttonContainer}>
+            <button className={style.expertise}>See Expertise</button>
+            <button className={style.lets}>
+              Let's Talk{" "}
+              <span className={style.arrow}>
+                <FaArrowRight />
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={style.animation}>
+        <Spline scene="https://prod.spline.design/hxkNHEALrO0u9MfH/scene.splinecode" />
       </div>
     </>
   );
