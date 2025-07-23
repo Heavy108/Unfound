f50e2869145757820de89979fdc393fdb34558eb
23f1000758 <heavy1089@gmail.com>
2025-07-23 17:22:17 +0530

    Navbar design + responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
 import Image from "next/image";
-import gradient1 from "@/assets/Gradient.png";
+import Navbar from "@/components/navbar";
+import Gradient1 from "@/assets/Gradient.webp";
+
 export default function Home() {
-  return(
+  return (
     <>
-    <Image src={gradient1} alt="gradient1"/>
+      <div className="relative  h-[500px] w-full overflow-hidden ">
+        {/* Background image */}
+        <Image src={Gradient1} alt="gradient1" fill className="z-0" priority />
+
+        {/* Navbar on top */}
+        <div className="relative z-10">
+          <Navbar />
+        </div>
+      </div>
     </>
-  )
+  );
 }
