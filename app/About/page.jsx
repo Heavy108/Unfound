3e65cc79005009e3797f1fdd5dee73cd08dead29
23f1000758 <heavy1089@gmail.com>
2025-08-19 23:55:35 +0530

    testing 4

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -4,12 +4,15 @@ import Feature from "../../components/Features";
 import Reasons from "../../components/Reason";
 import ReasonCard from "../../components/ReasonCard";
 import Services from "../../components/Services"
-// import photo from "../../assets/WebsiteDesign.png"
+import photo from "../../assets/WebsiteDesign.svg"
+import photo2 from "../../assets/VisualDesign.svg";
+
 import Image from "next/image";
 function About() {
   return (
     <>
-    {/* <Image src={photo} width={1200} height={800} alt="testing"/> */}
+      <Image src={photo} width={1200} height={800} alt="testing" />
+      <Image src={photo2} width={1200} height={800} alt="testing" />
     </>
   );
 }
